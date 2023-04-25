7cec210d2996a7bf3009f4accd334b4c27b7472c
rbqlsquf@konkuk.ac.kr <rbqlsquf@konkuk.ac.kr>
2023-04-25 14:56:33 +0900

    week7

--- week7/video_rgb.py
+++ week7/video_rgb.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+
 # cap = cv2.VideoCapture(0,cv2.CAP_MSMF)
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
@@ -7,11 +8,11 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 while True:
     ret, frame = cap.read()
-    if ret ==True:
-        b=frame[:,:,0]
-        g=frame[:,:,1]
-        r=frame[:,:,2]
-        rgb_frame = np.hstack((r,g,b))
+    if ret == True:
+        b = frame[:, :, 0]
+        g = frame[:, :, 1]
+        r = frame[:, :, 2]
+        rgb_frame = np.hstack((r, g, b))
         cv2.imshow("RGB-images", rgb_frame)
     key = cv2.waitKey(33)
     if key == ord('q'):
