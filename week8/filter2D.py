4031b71b48b6de5dc5e1f1e16bf2da86f9b4a7f2
sillyButNot <rbqlsquf@konkuk.ac.kr>
2023-04-25 15:48:37 +0900

    week8

---
+++ week8/filter2D.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+ksize = int(input('Enter kernel size:'))
+kernel = np.ones((ksize, ksize), np.float32) / (ksize * ksize)
+cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        filtered_frame = cv2.filter2D(frame, -1, kernel)
+        cframe = np.hstack((frame, filtered_frame))
+        cv2.imshow('Image', cframe)
+        key = cv2.waitKey(33)
+        if key == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
