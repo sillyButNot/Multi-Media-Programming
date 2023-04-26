4e48930a4bca7cff72856bc1da908bb1e05de5fd
sillyButNot <rbqlsquf@konkuk.ac.kr>
2023-04-26 12:39:50 +0900

    week8

---
+++ week8/color_filter.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+ksize = int(input('Enter kernel size:'))
+kernel = np.ones((ksize, ksize), np.float32) / (ksize * ksize)
+ngain = float(input('Enter noise gain:'))
+width = 320
+height = 240
+cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        t_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
+        Y, Cr, Cb = cv2.split(t_frame)
+        noisy = np.clip(Y + np.random.random((height, width)) * ngain, 0, 255).astype(np.uint8)
+        filtered = cv2.filter2D(noisy, -1, kernel)
+        cnoisy = cv2.cvtColor(cv2.merge((noisy, Cr, Cb)), cv2.COLOR_YCrCb2BGR)
+        cfiltered = cv2.cvtColor(cv2.merge((filtered, Cr, Cb)), cv2.COLOR_YCrCb2BGR)
+        cframe = np.hstack((frame, cnoisy, cfiltered))
+        cv2.imshow('Original, Noisy, Filtered', cframe)
+        key = cv2.waitKey(33)
+        if key == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
---
+++ week8/lowpass_filter.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+ksize = int(input('Enter kernel size:'))
+kernel = np.ones((ksize, ksize), np.float32) / (ksize * ksize)
+ngain = float(input('Enter noise gain:'))
+width = 320
+height = 240
+cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        noisy = np.clip(img + np.random.random((height, width)) * ngain, 0, 255).astype(np.uint8)
+        filtered_img = cv2.filter2D(noisy, -1, kernel)
+        cframe = np.hstack((img, noisy, filtered_img))
+        cv2.imshow('Original, Noisy, Filtered', cframe)
+        key = cv2.waitKey(33)
+        if key == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
